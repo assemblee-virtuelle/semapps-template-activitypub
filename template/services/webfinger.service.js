ab83493596715ed78b49dd6cb37effce5aeee117
s-ol <s-ol@users.noreply.github.com>
2021-01-28 18:46:05 +0100

    Update @semapps/webfinger settings (assemblee-virtuelle/semapps#539)

--- template/services/webfinger.service.js
+++ template/services/webfinger.service.js
@@ -4,6 +4,6 @@ const { WebfingerService } = require('@semapps/webfinger');
 module.exports = {
   mixins: [WebfingerService],
   settings: {
-    usersContainer: urlJoin(process.env.SEMAPPS_HOME_URL, 'actors')
+    baseUrl: process.env.SEMAPPS_HOME_URL
   }
 };
